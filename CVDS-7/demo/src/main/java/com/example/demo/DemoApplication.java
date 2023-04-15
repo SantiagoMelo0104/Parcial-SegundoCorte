cf2fa3210f9c01fe901892a86ed0df4a03b32c8c
SantiagoMelo0104 <123812833+SantiagoMelo0104@users.noreply.github.com>
2023-04-15 09:33:47 -0500

    Add files via upload

---
+++ CVDS-7/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.Local;
+import org.springframework.context.annotation.Bean;
+import java.time.LocalDate;
+
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@Autowired
+	EmployeeService employeeService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run() throws Exception {
+		return (args) -> {
+
+			System.out.println("\nDeleting employee....");
+			employeeService.deleteEmployeeAll();
+			System.out.println("Adding employees....\n");
+
+			employeeService.addEmployee(new Employee("Santiago", "Naranjo", "Student", 63000.00, LocalDate.of(2003, 4, 1)));
+			employeeService.addEmployee(new Employee("Jorge", "Useche", "Teacher", 43000.00,  LocalDate.of(1992, 1, 15)));
+
+			System.out.println("\nGetting all employees....");
+			employeeService.getAllEmployees().forEach(employee -> System.out.println(employee));
+
+			System.out.println("\nDeleting employee....");
+
+
+		};
+	}
+
+}
